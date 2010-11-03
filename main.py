8a33ebb383c8d16d95249f3c6aadcb01e70f5a44
Eric Entzel <eric@ubermac.net>
2010-11-03 13:01:58 -0600

    Generate authorization link for accessing Google Calendar

--- main.py
+++ main.py
@@ -18,12 +18,28 @@ from google.appengine.ext import webapp
 from google.appengine.ext.webapp import util
 from google.appengine.ext.webapp import template
 import os
+import atom.url
+import gdata.service
+import gdata.alt.appengine
+import settings
 
 
 class MainHandler(webapp.RequestHandler):
     def get(self):
         path = os.path.join(os.path.dirname(__file__), 'index.html')
-        self.response.out.write(template.render(path, {}))
+        template_values = { 'auth_link': self.auth_link() }
+        self.response.out.write(template.render(path, template_values))
+
+    def auth_link(self):
+        next_url = atom.url.Url('http', settings.HOST_NAME, path='/success')
+        client = gdata.service.GDataService()
+        gdata.alt.appengine.run_on_appengine(client)
+        return client.GenerateAuthSubURL(
+            next_url,
+            ('https://www.google.com/calendar/feeds/default/private/full',),
+            secure=False, session=True)
+
+
 
 
 def main():
