543bd9a4b882434cae163cb02dc66dd9f9cba619
Eric Entzel <eric@ubermac.net>
2010-11-07 16:16:42 -0700

    Factor out interation with Google APIs into a google_api module

---
+++ google_api.py
@@ -0,0 +1,38 @@
+import atom.url
+import gdata.service
+import gdata.alt.appengine
+import settings
+
+
+GCAL_FEED = 'https://www.google.com/calendar/feeds/default/private/full'
+
+
+def get_client():
+    client = gdata.service.GDataService()
+    gdata.alt.appengine.run_on_appengine(client)
+    return client
+    
+def generate_auth_link():
+    next_url = atom.url.Url('http', settings.HOST_NAME, path='/register')
+    client = get_client()
+    return client.GenerateAuthSubURL(next_url, GCAL_FEED, secure=False, session=True)
+
+def temp_token_from_url(url):
+    return gdata.auth.extract_auth_sub_token_from_url(url)
+    
+def permanent_token_from_temp_token(temp_token):
+    session_token = None
+    client = get_client()
+    session_token = client.upgrade_to_session_token(temp_token)
+    return session_token
+
+def quickadd_event_using_token(event, token_str):
+    client = get_client()
+    token = gdata.auth.AuthSubToken()
+    token.set_token_string(token_str)
+    client.current_token = token
+    xml_data = """<entry xmlns='http://www.w3.org/2005/Atom' xmlns:gCal='http://schemas.google.com/gCal/2005'>
+                      <content type="html">%s</content>
+                      <gCal:quickadd value="true"/>
+                  </entry>""" % (event)
+    return client.Post(xml_data, GCAL_FEED)
--- main.py
+++ main.py
@@ -6,30 +6,19 @@ from google.appengine.ext import webapp
 from google.appengine.ext.webapp import util
 from google.appengine.ext.webapp import template
 import os
-import atom.url
-import gdata.service
-import gdata.alt.appengine
-import settings
 import logging
 import uuid
+import google_api
 from ecal_users import EmailUser
-
-GCAL_FEED = 'https://www.google.com/calendar/feeds/default/private/full'
 
 
 class RegistrationHandler(webapp.RequestHandler):
     def get(self):
-        client = gdata.service.GDataService()
-        gdata.alt.appengine.run_on_appengine(client)
-        session_token = None
-        auth_token = gdata.auth.extract_auth_sub_token_from_url(self.request.uri)
-        if auth_token == None:
+        temp_token = google_api.temp_token_from_url(self.request.uri)
+        if temp_token == None:
             logging.error("Couldn't extract one-time auth token from URL")
             return
-        session_token = client.upgrade_to_session_token(auth_token)
-        if session_token == None:
-            logging.error("Couldn't upgrade to session token")
-            return
+        session_token = google_api.permanent_token_from_temp_token(temp_token)
         myuser = EmailUser()
         myuser.email_address = str(uuid.uuid4())
         myuser.auth_token = str(session_token)
@@ -41,37 +30,17 @@ class RegistrationHandler(webapp.RequestHandler):
     
 class AddEventHandler(webapp.RequestHandler):
     def get(self):
-        client = gdata.service.GDataService()
-        gdata.alt.appengine.run_on_appengine(client)
         all_users = EmailUser.gql("ORDER BY date_added DESC LIMIT 1")
         for my_user in all_users:
             token_str = my_user.auth_token.split('=')[1]
-        token = gdata.auth.AuthSubToken()
-        token.set_token_string(token_str)
-        client.current_token = token
-        xml_data = """<entry xmlns='http://www.w3.org/2005/Atom' xmlns:gCal='http://schemas.google.com/gCal/2005'>
-                          <content type="html">Tennis at Hyland Feb 11 3p-3:30</content>
-                          <gCal:quickadd value="true"/>
-                      </entry>"""
-        response = client.Post(xml_data, GCAL_FEED)
+        response = google_api.quickadd_event_using_token('Tennis at Hyland Feb 11 3p-3:30', token_str)
         self.response.out.write(response)
 
 class MainHandler(webapp.RequestHandler):
     def get(self):
         path = os.path.join(os.path.dirname(__file__), 'index.html')
-        template_values = { 'auth_link': self.auth_link() }
+        template_values = { 'auth_link': google_api.generate_auth_link() }
         self.response.out.write(template.render(path, template_values))
-
-    def auth_link(self):
-        next_url = atom.url.Url('http', settings.HOST_NAME, path='/register')
-        client = gdata.service.GDataService()
-        gdata.alt.appengine.run_on_appengine(client)
-        return client.GenerateAuthSubURL(
-            next_url,
-            (GCAL_FEED,),
-            secure=False, session=True)
-
-
 
 
 def main():
