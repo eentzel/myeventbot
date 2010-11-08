cf013fcbb705a5d68948846e28e6ea55ce04b05a
Eric Entzel <eric@ubermac.net>
2010-11-07 17:15:33 -0700

    raise, not throw...

--- google_api.py
+++ google_api.py
@@ -20,7 +20,7 @@ def generate_auth_link():
 def temp_token_from_url(url):
     token = gdata.auth.extract_auth_sub_token_from_url(url)
     if token == None:
-        throw Exception("Couldn't extract one-time auth token from URL")
+        raise Exception("Couldn't extract one-time auth token from URL")
     return token
 
 def permanent_token_from_temp_token(temp_token):
