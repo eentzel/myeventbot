40ac5bc5f8c3f68c0c5b2b6debe19b487893d6f5
Eric Entzel <eric@ubermac.net>
2010-11-09 11:10:08 -0700

    Use a slightly friendlier string than UUID for email addresses.

--- ecal_users.py
+++ ecal_users.py
@@ -1,9 +1,26 @@
 from google.appengine.ext import db
-import uuid
+import random
+import string
 
+def make_address():
+    """
+    Returns a random alphanumeric string of 10 digits.  Since
+    there are 62 choices per digit, this gives:
+    62 ** 10 = 8.39299366 x 10 ** 17
+
+    possible results.  When there are a million accounts active,
+    we need:
+    10 ** 6 x 10 ** 6 = 10 ** 12
+
+    possible results to have a one-in-a-million chance of a
+    collision, so this seems like a safe number.
+    """
+    chars = string.letters + string.digits
+    return ''.join([ random.choice(chars) for i in range(10) ])
+        
 class EmailUser(db.Model):
     # the email address that the user sends events to:
-    email_address = db.StringProperty(default=str(uuid.uuid4()))
+    email_address = db.StringProperty(default=make_address())
     # the AuthSub token used to authenticate the user to gcal:
     auth_token = db.StringProperty()
     date_added = db.DateTimeProperty(auto_now_add=True)
