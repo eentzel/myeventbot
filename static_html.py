ab692dc95811b9744dd1dbc9689599be39ce6eeb
Eric Entzel <eric@ubermac.net>
2010-11-10 14:12:10 -0700

    Create a generic static file handler.

---
+++ static_html.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+#
+# Copyright 2010 Eric Entzel <eric@ubermac.net>
+#
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp import util
+from google.appengine.ext.webapp import template
+import os
+
+
+class StaticHandler(webapp.RequestHandler):
+    def get(self, path):
+        full_path = os.path.join(os.path.dirname(__file__), path)
+        self.response.out.write(template.render(full_path, None))
+
+
+def main():
+    application = webapp.WSGIApplication([(r'/(.*)', StaticHandler)],
+                                         debug = (os.environ['SERVER_NAME'] == 'localhost'))
+    util.run_wsgi_app(application)
+
+
+if __name__ == '__main__':
+    main()
