1a800f394a5f840e8f89821b15601a1292b379d1
Eric Entzel <eric@ubermac.net>
2010-11-17 15:52:24 -0700

    Factor out the part of quickadd_event_using_token() that sets up the token, so it can be used by other future methods that needs to access the GCal API using a token

--- google_api.py
+++ google_api.py
@@ -18,7 +18,17 @@ def get_client():
     client = gdata.service.GDataService()
     gdata.alt.appengine.run_on_appengine(client)
     return client
-    
+
+def get_client_with_token(token_str):
+    client = get_client()
+    if debug:
+        token = gdata.auth.AuthSubToken()
+    else:
+        token = gdata.auth.SecureAuthSubToken(rsa_key)
+    token.set_token_string(token_str)
+    client.current_token = token
+    return client
+
 def generate_auth_link():
     next_url = atom.url.Url('https', settings.HOST_NAME, path='/register')
     client = get_client()
@@ -34,13 +44,7 @@ def permanent_token_from_temp_token(temp_token):
     return session_token
 
 def quickadd_event_using_token(event, token_str):
-    client = get_client()
-    if debug:
-        token = gdata.auth.AuthSubToken()
-    else:
-        token = gdata.auth.SecureAuthSubToken(rsa_key)
-    token.set_token_string(token_str)
-    client.current_token = token
+    client = get_client_with_token(token_str)
     xml_data = """<entry xmlns='http://www.w3.org/2005/Atom' xmlns:gCal='http://schemas.google.com/gCal/2005'>
                       <content type="html">%s</content>
                       <gCal:quickadd value="true"/>
