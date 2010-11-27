f31311dddc8376b70aae69c094b6856e13bd47d1
Eric Entzel <eric@ubermac.net>
2010-11-26 20:58:25 -0700

    Show an error message when the datastore is in read-only mode.

--- registration.py
+++ registration.py
@@ -5,6 +5,7 @@
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import util
 from google.appengine.ext.webapp import template
+from google.appengine.runtime import apiproxy_errors
 import os
 import google_api
 from ecal_users import EmailUser
@@ -12,6 +13,14 @@ from ecal_wsgi import EcalWSGIApplication
 
 
 class RegistrationHandler(webapp.RequestHandler):
+    def datastore_down_error(self):
+        template_values = {
+            'title': 'Unable to create account',
+            'error_text': "We couldn't create your account right now because the database is in read-only mode for maintenance.  Please try back in a few minutes."
+        }
+        path = os.path.join(os.path.dirname(__file__), 'error.html')
+        self.response.out.write(template.render(path, template_values))
+
     def get(self):
         temp_token = google_api.temp_token_from_url(self.request.uri)
         if temp_token == None:
@@ -19,7 +28,11 @@ class RegistrationHandler(webapp.RequestHandler):
             return
         session_token = google_api.permanent_token_from_temp_token(temp_token)
         myuser = EmailUser(auth_token=session_token.get_token_string())
-        myuser.put()
+        try:
+            myuser.put()
+        except apiproxy_errors.CapabilityDisabledError:
+            self.datastore_down_error()
+            return
         template_values = { 'email_address': myuser.email_address + '@' + os.environ['APPLICATION_ID'] + '.appspotmail.com' }
         path = os.path.join(os.path.dirname(__file__), 'success.html')
         self.response.out.write(template.render(path, template_values))            
