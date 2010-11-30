2137567ca98f660d0cedf9a85bbffc20cd87d7ac
Eric Entzel <eric@ubermac.net>
2010-11-30 13:37:40 -0700

    Refactor resgistration handler.

--- registration.py
+++ registration.py
@@ -10,27 +10,30 @@ import ecal
 
 
 class RegistrationHandler(ecal.EcalRequestHandler):
-    def datastore_down_error(self):
-        template_values = {
-            'title': 'Unable to create account',
-            'error_text': "We couldn't create your account right now because the database is in read-only mode for maintenance.  Please try back in a few minutes."
-        }
-        self.respond_with_template('error.html', template_values)
+    def authorization_denied(self):
+        self.redirect("/")
 
-    def get(self):
-        temp_token = google_api.temp_token_from_url(self.request.uri)
-        if temp_token == None:
-            self.redirect("/")
-            return
-        session_token = google_api.permanent_token_from_temp_token(temp_token)
+    def authorization_succeeded(self):
+        session_token = google_api.permanent_token_from_temp_token(self.temp_token)
         myuser = ecal.EcalUser(auth_token=session_token.get_token_string())
         try:
             myuser.put()
         except apiproxy_errors.CapabilityDisabledError:
-            self.datastore_down_error()
+            template_values = {
+                'title': 'Unable to create account',
+                'error_text': "We couldn't create your account right now because the database is in read-only mode for maintenance.  Please try back in a few minutes."
+                }
+            self.respond_with_template('error.html', template_values)
             return
         template_values = { 'email_address': myuser.email_address + '@' + os.environ['APPLICATION_ID'] + '.appspotmail.com' }
         self.respond_with_template('success.html', template_values)
+
+    def get(self):
+        self.temp_token = google_api.temp_token_from_url(self.request.uri)
+        if self.temp_token != None:
+            self.authorization_succeeded()
+        else:
+            self.authorization_denied()
 
 
 def main():
