fd716946d61df66a0b4a63e5e1c0ced3aaa6913a
Eric Entzel <eric@ubermac.net>
2010-11-29 21:58:41 -0700

    Remove files that were only needed for migration.

--- email_user.py
+++
@@ -1,7 +0,0 @@
-from google.appengine.ext import db
-
-class EmailUser(db.Model):
-    email_address = db.StringProperty()
-    auth_token = db.StringProperty()
-    date_added = db.DateTimeProperty()
-    last_action = db.DateTimeProperty()
