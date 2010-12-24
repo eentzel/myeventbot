23ce9dd6a19c093c4a34bcba2ec46fc24b093606
Eric Entzel <eric@ubermac.net>
2010-12-23 21:14:48 -0700

    Script to deploy to App Engine version based on branch name

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+BRANCH=`git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'`
+
+if [ "$BRANCH" != "master" ] && [ "$BRANCH" != "staging" ]
+then
+    echo "I don't know how to deploy branch '${BRANCH}' - only 'staging' and 'master'"
+    exit
+fi
+
+if [ "$BRANCH" == "master" ]
+then
+    read -n 1 -p "Really deploy branch '${BRANCH}' to production? " CONFIRM
+    if [ "$CONFIRM" != "y" ]
+    then
+        echo
+        exit
+    fi
+    echo
+fi
+
+echo "Uploading branch '${BRANCH}' to App Engine"
+appcfg.py update . --version $BRANCH
