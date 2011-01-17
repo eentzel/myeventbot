814b45f5171a96832345081e1df2e95d4f8b4a4a
Eric Entzel <eric@ubermac.net>
2011-01-16 19:27:57 -0700

    Sketelon for an admin dashboard

---
+++ dashboard.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+#
+# Copyright 2010 Eric Entzel <eric@ubermac.net>
+#
+import datetime
+from google.appengine.ext.webapp import util
+
+import ecal
+
+
+class DashboardHandler(ecal.EcalRequestHandler):
+    def get(self):
+        one_day_ago = datetime.datetime.now() - datetime.timedelta(days=1)
+        query = ecal.EcalUser.all().filter('date_added >=', one_day_ago)
+        self.respond_with_template('dashboard.html', {'signups': query.count()})
+        pass
+
+
+def main():
+    application = ecal.EcalWSGIApplication([('/dashboard', DashboardHandler)])
+    util.run_wsgi_app(application)
+
+
+if __name__ == '__main__':
+    main()
