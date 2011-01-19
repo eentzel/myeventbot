10a2abe4e91b656ae6b051702122f3d7b7fadf83
Eric Entzel <eric@ubermac.net>
2011-01-18 21:46:47 -0700

    Add daily unique users to the dashboard

--- dashboard.py
+++ dashboard.py
@@ -12,6 +12,12 @@ DAYS_OF_HISTORY = 7
 
     
 class DashboardHandler(ecal.EcalRequestHandler):
+    def unique_users(self):
+        users = ecal.EcalUser.all().filter('last_action >=', self.days[-1])
+        retval = []
+        for day in self.days:
+            retval.append(len([u for u in users if u.last_action - day < self.one_day and u.last_action - day >= datetime.timedelta(0)]))
+        return retval
     
     def signups(self):
         signups = ecal.EcalUser.all().filter('date_added >=', self.days[-1])
@@ -29,7 +35,10 @@ class DashboardHandler(ecal.EcalRequestHandler):
                                                         second=0, microsecond=0)
         self.one_day = datetime.timedelta(days=1)
         self.days = [today_start - i * self.one_day for i in xrange(0, DAYS_OF_HISTORY)]
-        self.respond_with_template('dashboard.html', {'signups': self.signups(), 'days': [self.format_day(d) for d in self.days]})
+        self.respond_with_template('dashboard.html', {
+                'signups': self.signups(),
+                'days': [self.format_day(d) for d in self.days],
+                'uniques': self.unique_users()})
 
 
 def main():
