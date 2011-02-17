9e2e86b15fb9d96f370aff7000d5c48b8225f7be
Eric Entzel <eric@ubermac.net>
2011-02-17 15:21:27 -0700

    Configure Django version explicitly, as directed by log warnings and http://code.google.com/appengine/docs/python/tools/libraries.html#Django
    Not 100% sure about the gaeunit bit, but it seems to work

--- lib/gaeunit.py
+++ lib/gaeunit.py
@@ -60,6 +60,11 @@ __url__ = "http://code.google.com/p/gaeunit"
 
 import sys
 import os
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+from google.appengine.dist import use_library
+use_library('django', '1.2')
+
 import unittest
 import time
 import logging
