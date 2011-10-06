0df7212e6ba2652ad9d7f01d43ef45811c5a02de
Eric Entzel <eric@ubermac.net>
2011-10-06 23:09:45 +1100

    /authorize URL doesn't need to be SSL. Also roll its handler into RegistrationHandler

--- registration.py
+++ registration.py
@@ -29,20 +29,18 @@ class RegistrationHandler(ecal.EcalRequestHandler):
         self.respond_with_template('success.html', template_values)
 
     def get(self):
-        self.temp_token = google_api.temp_token_from_url(self.request.uri)
-        if self.temp_token != None:
-            self.authorization_succeeded()
+        if self.request.path == '/authorize':
+            self.redirect(google_api.generate_auth_link())
         else:
-            self.authorization_denied()
-
-class SendToAuth(ecal.EcalRequestHandler):
-    def get(self):
-        self.redirect(google_api.generate_auth_link())
+            self.temp_token = google_api.temp_token_from_url(self.request.uri)
+            if self.temp_token != None:
+                self.authorization_succeeded()
+            else:
+                self.authorization_denied()
 
 
 def main():
-    application = ecal.EcalWSGIApplication([('/register', RegistrationHandler),
-                                            ('/authorize', SendToAuth)])
+    application = ecal.EcalWSGIApplication([('/.*', RegistrationHandler)])
     util.run_wsgi_app(application)
 
 
